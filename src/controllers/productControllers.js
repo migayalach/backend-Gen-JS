044b795ca3c5fcae30ce9e2531ec9a62b06f4272
migayalach <ayalachavezmiguel@gmail.com>
2024-12-03 09:39:32 -0400

    product controller!

---
+++ src/controllers/productControllers.js
@@ -0,0 +1,27 @@
+const productCreate = () => {
+  return "Product create";
+};
+
+const getAllProducts = () => {
+  return "Product all";
+};
+
+const getIdProducts = () => {
+  return "Product id";
+};
+
+const deleteProduct = () => {
+  return "Product delete";
+};
+
+const updateProduct = () => {
+  return "Product update";
+};
+
+module.exports = {
+  productCreate,
+  getAllProducts,
+  getIdProducts,
+  deleteProduct,
+  updateProduct,
+};
