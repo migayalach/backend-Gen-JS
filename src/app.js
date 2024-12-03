84311eff511faeef57e6067cd6455b5b5ddb63a5
migayalach <ayalachavezmiguel@gmail.com>
2024-12-03 09:38:42 -0400

    app js

---
+++ src/app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const mainRouter = require("./routes");
+const app = express();
+
+app.use(express.json());
+app.use(morgan("dev"));
+app.use(cors());
+app.use("/api", mainRouter);
+
+module.exports = app;
