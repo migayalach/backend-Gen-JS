bfd4869b8ba4534564b9ba34ea136bab13a4278b
migayalach <ayalachavezmiguel@gmail.com>
2024-12-03 12:48:58 -0400

    src/routes/index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,11 +6,13 @@ const entryExitRouter = require("./entryExitRoute");
 const loginRouter = require("./loginRoute");
 const productRouter = require("./productRoute");
 const userRouter = require("./userRouter");
+const auditProductRouter = require("./productAuditRoute");
 
 // ENTRY POINT'S
-mainRouter.use("/entryExit", entryExitRouter);
-mainRouter.use("/login", loginRouter);
-mainRouter.use("/product", productRouter);
 mainRouter.use("/user", userRouter);
+mainRouter.use("/login", loginRouter);
+mainRouter.use("/entryExit", entryExitRouter);
+mainRouter.use("/product", productRouter);
+mainRouter.use("/auditProduct", auditProductRouter);
 
 module.exports = mainRouter;
