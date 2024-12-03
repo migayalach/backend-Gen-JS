f6e31272e55a49aa65c6e1edb1e9c23ee199fa91
migayalach <ayalachavezmiguel@gmail.com>
2024-12-03 12:49:42 -0400

    src/controllers/usersControllers.js

--- src/controllers/usersControllers.js
+++ src/controllers/usersControllers.js
@@ -1,9 +1,30 @@
-const userCreate = () => {
-  return "create user";
+const pool = require("../database/conexion");
+const { resAllUser, hashedPassword } = require("../utils/userUtils");
+
+const userGetAll = async () => {
+  const [data] = await pool.query(
+    `SELECT idUser, nameUser, lastNameUser, emailUser FROM user`
+  );
+  return resAllUser(data);
 };
 
-const userGetAll = () => {
-  return "all users";
+const userCreate = async (nameUser, lastNameUser, emailUser, passwordUser) => {
+  const [emailExist] = await pool.query(
+    `SELECT emailUser FROM user WHERE emailUser = ?`,
+    [emailUser]
+  );
+
+  if (emailExist.length) {
+    throw Error(`Este email ya se encuetra registrado`);
+  }
+
+  const password = await hashedPassword(passwordUser);
+
+  await pool.query(
+    `INSERT INTO user (nameUser, lastNameUser, emailUser, passwordUser) VALUES(?, ?, ?, ?)`,
+    [nameUser, lastNameUser, emailUser, password]
+  );
+  return { state: "create-user", data: await userGetAll() };
 };
 
 module.exports = { userCreate, userGetAll };
