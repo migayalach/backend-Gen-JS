c456a91465730aa61af3da54f3e1acffc6ae9d2d
migayalach <ayalachavezmiguel@gmail.com>
2024-12-03 12:49:29 -0400

    src/controllers/productControllers.js

--- src/controllers/productControllers.js
+++ src/controllers/productControllers.js
@@ -1,21 +1,98 @@
-const productCreate = () => {
-  return "Product create";
+const pool = require("../database/conexion");
+
+const productCreate = async (
+  nameProduct,
+  codeProduct,
+  priceProduct,
+  urlProduct,
+  stockProduct,
+  madeProduct,
+  sizeProduct,
+  dateIntroProduct
+) => {
+  const [codeExist] = await pool.query(
+    `SELECT codeProduct FROM product WHERE codeProduct = ?`,
+    [codeProduct]
+  );
+
+  if (codeExist.length) {
+    throw Error(`Este codigo ya se encuentra registrado`);
+  }
+
+  const [data] = await pool.query(
+    `INSERT INTO product (nameProduct, codeProduct, priceProduct, urlProduct, stockProduct, madeProduct, sizeProduct, dateIntroProduct) VALUES (?, ?, ?, ?, ?, ?, ?, ?)`,
+    [
+      nameProduct,
+      codeProduct,
+      priceProduct,
+      urlProduct,
+      stockProduct,
+      madeProduct,
+      sizeProduct,
+      dateIntroProduct,
+    ]
+  );
+
+  return { state: "create-product", data: await getAllProducts() };
 };
 
-const getAllProducts = () => {
-  return "Product all";
+const getAllProducts = async () => {
+  const [data] = await pool.query(`SELECT * FROM product`);
+  return data;
 };
 
-const getIdProducts = () => {
-  return "Product id";
+const getIdProducts = async (idProduct) => {
+  const [data] = await pool.query(`SELECT * FROM product WHERE idProduct = ?`, [
+    idProduct,
+  ]);
+  if (!data.length) {
+    throw Error(`El producto buscado no existe`);
+  }
+  return data[0];
 };
 
-const deleteProduct = () => {
-  return "Product delete";
+const deleteProduct = async (idProduct) => {
+  await getIdProducts(idProduct);
+  await pool.query(`DELETE FROM product WHERE idProduct = ? `, [idProduct]);
+  return { state: "product-delete" };
 };
 
-const updateProduct = () => {
-  return "Product update";
+const updateProduct = async (
+  idProduct,
+  nameProduct,
+  codeProduct,
+  priceProduct,
+  urlProduct,
+  stockProduct,
+  madeProduct,
+  sizeProduct,
+  dateIntroProduct,
+  stateProduct
+) => {
+  await getIdProducts(idProduct);
+  const [codeExist] = await pool.query(
+    `SELECT codeProduct FROM product WHERE codeProduct = ? AND idProduct != ? `,
+    [codeProduct, idProduct]
+  );
+  if (codeExist.length) {
+    throw Error(`Este codigo ya se encuentra registrado`);
+  }
+  await pool.query(
+    `UPDATE product SET nameProduct = ?, codeProduct = ?, priceProduct = ?, urlProduct = ?, stockProduct = ?, madeProduct = ?, sizeProduct = ?, dateIntroProduct = ?, stateProduct = ? WHERE idProduct = ?`,
+    [
+      nameProduct,
+      codeProduct,
+      priceProduct,
+      urlProduct,
+      stockProduct,
+      madeProduct,
+      sizeProduct,
+      dateIntroProduct,
+      stateProduct,
+      idProduct,
+    ]
+  );
+  return getIdProducts(idProduct);
 };
 
 module.exports = {
