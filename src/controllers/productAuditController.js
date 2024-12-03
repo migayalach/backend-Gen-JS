b6465ad5e40e424244ef34858442d98f0c461843
migayalach <ayalachavezmiguel@gmail.com>
2024-12-03 13:13:43 -0400

    src/controllers/productAuditController.js

--- src/controllers/productAuditController.js
+++ src/controllers/productAuditController.js
@@ -1,19 +1,33 @@
 const pool = require("../database/conexion");
+const { clearAuditProduct } = require("../utils/productAuditUtils");
 
 const addAuditProduct = async (
   idProduct,
   idUser,
   idAction,
-  timeAction,
   oldData,
   newData
 ) => {
   const currentDate = new Date();
-  return "add";
+  await pool.query(
+    `INSERT INTO productAction (idProduct, idUser, idAction, timeAction, oldData, newData) VALUES (?, ?, ?, ?, ?, ?)`,
+    [
+      idProduct,
+      idUser,
+      idAction,
+      currentDate,
+      JSON.stringify(oldData),
+      JSON.stringify(newData),
+    ]
+  );
+  return;
 };
 
 const getAllAuditProduct = async () => {
-  return "all";
+  const [data] = await pool.query(
+    `SELECT p.nameProduct, p.codeProduct, u.nameUser, a.nameAction, pa.timeAction, pa.oldData, pa.newData FROM user u, product p, actions a, productAction pa WHERE pa.idProduct = p.idProduct AND pa.idUser = u.idUser AND pa.idAction = a.idAction`
+  );
+  return clearAuditProduct(data);
 };
 
 module.exports = {
