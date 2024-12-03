029782c9e9091f7f945d75d2d20cd6ea8c2ff2d4
migayalach <ayalachavezmiguel@gmail.com>
2024-12-03 12:48:03 -0400

    src/utils/userUtils.js

---
+++ src/utils/userUtils.js
@@ -0,0 +1,23 @@
+const bcrypt = require("bcrypt");
+
+function encryEmail(email) {
+  const [localPart, domain] = email.split("@");
+  const maskedLocalPart =
+    localPart.slice(0, 2) +
+    "*".repeat(Math.max(localPart.length - 4, 0)) +
+    localPart.slice(-2);
+  return `${maskedLocalPart}@${domain}`;
+}
+
+function resAllUser(list) {
+  return list.map(({ idUser, nameUser, lastNameUser, emailUser }) => ({
+    idUser,
+    nameUser,
+    lastNameUser,
+    emailUser: encryEmail(emailUser),
+  }));
+}
+
+const hashedPassword = async (password) => await bcrypt.hash(password, 10);
+
+module.exports = { resAllUser, hashedPassword };
