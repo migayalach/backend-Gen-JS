55d4685e0e40719eee443f252dd35ec68208a4d9
migayalach <ayalachavezmiguel@gmail.com>
2024-12-03 12:47:37 -0400

    src/controllers/entryExitController.js

--- src/controllers/entryExitController.js
+++ src/controllers/entryExitController.js
@@ -1,9 +1,20 @@
-const postRegActivity = async () => {
-  return "registro actividad";
+const pool = require("../database/conexion");
+
+const postRegActivity = async (idUser, idEntryExit) => {
+  const currentDate = new Date();
+  await pool.query(
+    `INSERT INTO entryExitUser (idUser, idEntryExit, timeEntryExit) VALUES (?,?,?)`,
+    [idUser, idEntryExit, currentDate]
+  );
+
+  return;
 };
 
 const getAllActivity = async () => {
-  return "gel all activity";
+  const [data] = await pool.query(
+    `SELECT u.nameUser, ex.nameEntryExit, e.timeEntryExit FROM entryExitUser e, user u, entryExit ex WHERE e.idUser = u.idUser AND e.idEntryExit = ex.idEntryExit ORDER BY e.timeEntryExit DESC`
+  );
+  return data;
 };
 
 module.exports = { postRegActivity, getAllActivity };
