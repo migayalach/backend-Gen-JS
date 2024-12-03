dca2f253897026edb7cdd87d39e920715f0df4c8
migayalach <ayalachavezmiguel@gmail.com>
2024-12-03 09:39:00 -0400

    entryExitController

---
+++ src/controllers/entryExitController.js
@@ -0,0 +1,9 @@
+const postRegActivity = async () => {
+  return "registro actividad";
+};
+
+const getAllActivity = async () => {
+  return "gel all activity";
+};
+
+module.exports = { postRegActivity, getAllActivity };
