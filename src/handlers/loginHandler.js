422fc782e988890dc82ee96dbd802f8245c5a5af
migayalach <ayalachavezmiguel@gmail.com>
2024-12-03 12:50:28 -0400

    src/handlers/loginHandler.js

--- src/handlers/loginHandler.js
+++ src/handlers/loginHandler.js
@@ -1,8 +1,9 @@
 const { loginApp } = require("../controllers/loginController");
 
-const accessLogin = async(request, response) => {  
+const accessLogin = async (request, response) => {
+  const { emailUser, passwordUser } = request.body;
   try {
-    const data = await loginApp();
+    const data = await loginApp(emailUser, passwordUser);
     response.status(200).json(data);
   } catch (error) {
     response.status(400).json({ message: error.message, access: false });
