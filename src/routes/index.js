3eb062921c2c5d2cdff91169e3b28bd455247c5e
migayalach <ayalachavezmiguel@gmail.com>
2024-12-03 10:16:45 -0400

    index roter

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,13 +2,13 @@ const { Router } = require("express");
 const mainRouter = Router();
 
 // ROUTES
-const extryExitRouter = require("./extryExitRoute");
+const entryExitRouter = require("./entryExitRoute");
 const loginRouter = require("./loginRoute");
 const productRouter = require("./productRoute");
 const userRouter = require("./userRouter");
 
 // ENTRY POINT'S
-mainRouter.use("/entryExit", extryExitRouter);
+mainRouter.use("/entryExit", entryExitRouter);
 mainRouter.use("/login", loginRouter);
 mainRouter.use("/product", productRouter);
 mainRouter.use("/user", userRouter);
