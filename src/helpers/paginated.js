2e68d273d64a4d38d913ba4dc39db30a50732f33
migayalach <ayalachavezmiguel@gmail.com>
2024-12-06 13:16:25 -0400

    src/helpers/paginated.js

---
+++ src/helpers/paginated.js
@@ -0,0 +1,25 @@
+require("dotenv").config();
+const URL = process.env.URL;
+
+const nextRequest = (pages, page, site) => {
+  if (page < pages) {
+    return `${URL}${site}?page=${+page + 1}`;
+  }
+  return null;
+};
+
+const prevRequest = (page, site) => {
+  if (+page === 1) {
+    return null;
+  }
+  return `${URL}${site}?page=${+page - 1}`;
+};
+
+function navegation(pages, page, site) {
+  return {
+    next: nextRequest(pages, page, site),
+    prev: prevRequest(page, site),
+  };
+}
+
+module.exports = navegation;
