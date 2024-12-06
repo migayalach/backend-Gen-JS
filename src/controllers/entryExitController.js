4eedd0b2acf5a25179c8c4bd18b1805991719fd4
migayalach <ayalachavezmiguel@gmail.com>
2024-12-06 16:27:57 -0400

    src/controllers/entryExitController.js

--- src/controllers/entryExitController.js
+++ src/controllers/entryExitController.js
@@ -1,6 +1,7 @@
 const pool = require("../database/conexion");
+const responseData = require("../utils/response");
 
-const postRegActivity = async (idUser, idEntryExit) => { 
+const postRegActivity = async (idUser, idEntryExit) => {
   const currentDate = new Date();
   await pool.query(
     `INSERT INTO entryExitUser (idUser, idEntryExit, timeEntryExit) VALUES (?,?,?)`,
@@ -10,11 +11,14 @@ const postRegActivity = async (idUser, idEntryExit) => {
   return;
 };
 
-const getAllActivity = async () => {
+const getAllActivity = async (page) => {
+  if (!page) {
+    page = 1;
+  }
   const [data] = await pool.query(
     `SELECT u.nameUser, ex.nameEntryExit, e.timeEntryExit FROM entryExitUser e, user u, entryExit ex WHERE e.idUser = u.idUser AND e.idEntryExit = ex.idEntryExit ORDER BY e.timeEntryExit DESC`
   );
-  return data;
+  return responseData(data, page, "entryExit?");
 };
 
 module.exports = { postRegActivity, getAllActivity };
