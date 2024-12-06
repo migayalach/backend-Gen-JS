7631525215d45358f98e9ea6b9488f359dfec3ff
migayalach <ayalachavezmiguel@gmail.com>
2024-12-06 16:28:09 -0400

    src/controllers/levelController.js

--- src/controllers/levelController.js
+++ src/controllers/levelController.js
@@ -1,8 +1,9 @@
 const pool = require("../database/conexion");
+const responseData = require("../utils/response");
 
 const getAllLevel = async () => {
   const [data] = await pool.query(`SELECT * FROM level`);
-  return data;
+  return responseData(data, (page = 1), "level");
 };
 
 module.exports = {
